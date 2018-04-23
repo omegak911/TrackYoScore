cfd0197a6bb9d81105cd4051be515f656d46b764
Kin Chan <xkinchanx@gmail.com>
2018-04-23 14:12:07 -0700

    [add] db connection and table setup

--- database/SQL/index.jsx
+++ database/SQL/index.jsx
@@ -1,3 +1,36 @@
 //create functions to make and drop tables here
 //then update setup file so we can build and drop ASYNC as needed
 //create dummy data as needed
+
+import Sequelize from 'sequelize';
+import db from '../index';
+
+const Games = db.define('games', {
+  title: { type: Sequelize.STRING, unique: true },
+});
+
+const History = db.define('histories', {
+  gameID: Sequelize.INTEGER,
+  playerScore: Sequelize.JSON,
+});
+
+const HistoryConfirmation = db.define('confirmation', {
+  gameID: Sequelize.INTEGER,
+  playerScore: Sequelize.JSON,
+})
+
+const Perks = db.define('perks', {
+  type: { type: Sequelize.STRING, unique: true },
+});
+
+const Users = db.define('users', {
+  name: { type: Sequelize.STRING, unique: true },
+  password: Sequelize.STRING,
+  level: Sequelize.INTEGER,
+  currentEXP: Sequelize.INTEGER,
+  nextLevelEXP: Sequelize.INTEGER,
+  wins: Sequelize.INTEGER,
+  losses: Sequelize.INTEGER,
+});
+
+export { Games, History, HistoryConfirmation, Perks, Users };
