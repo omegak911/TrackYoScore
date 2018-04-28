cbe1780dfc1b3e8ed71c4b851b66f16ceadbf19c
Kin Chan <xkinchanx@gmail.com>
2018-04-28 04:17:30 -0700

    [add] addConfirmation function and pseudocode

--- server/database/src/Histories/HistoryCtrl.jsx
+++ server/database/src/Histories/HistoryCtrl.jsx
@@ -1,11 +1,38 @@
-import { addHistoryHelper, addTempHistoryHelper } from './HistoryHelper';
+import { 
+  addHistoryHelper, 
+  addConfirmationHelper, 
+  addUserConfirmationHelper, 
+  doesConfirmationExistHelper, 
+  fetchHistoryHelper, 
+  validateConfirmationHelper }from './HistoryHelper';
 
-const addHistory = () => {
+const addConfirmation = (req,res) => {
+  //check if user is logged in
+  addConfirmationHelper(req.body, (result) => {
+    //for each user, add to temp hist join table for confirmation
+    // const players = Object.keys(/* players */);
+    // for (let i = 0; i < players.length; i++) {
+    //   addUserConfirmationHelper(/* object.userID, object.historyConfirmationID*/)
+    // }
+    res.status(201).send('success')
+  })
+  //on success, client side should update user notification just for that user
+}
+
+const validateConfirmation = (req, res) => {
+  //check if user is logged in
+
+  validateConfirmationHelper
 
 }
 
-const addTempHistoryHelper = () => {
+
+const addHistory = (req, res) => {
+  //this function should be used by updat Temp Hist
+}
+
+const fetchHistory = (req, res) => {
 
 }
 
-export {  }
+export { addConfirmation, fetchHistory };
