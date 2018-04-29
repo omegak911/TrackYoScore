4cdec1d84154bcfbfd09fd9b51e1fe1f403b139e
Kin Chan <xkinchanx@gmail.com>
2018-04-29 11:46:24 -0700

    [add] routes for each function in HistoryCtrl

--- server/database/src/Histories/HistoryRouter.jsx
+++ server/database/src/Histories/HistoryRouter.jsx
@@ -1,9 +1,21 @@
 import express from 'express';
 
-// import {}
+import { 
+  addConfirmation, 
+  validateConfirmation, 
+  doesConfirmationExist, 
+  fetchHistory 
+} from './HistoryCtrl';
 
 const router = express.Router();
 
+router.route('/confirmation')
+  .get(doesConfirmationExist)
+  .post(addConfirmation)
+  .put(validateConfirmation);
+
+router.route('/history')
+  .get(fetchHistory);
 
 
 
