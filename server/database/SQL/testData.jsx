451a801ac0e30c5ded6069efdabdb580be7084c9
Kin Chan <xkinchanx@gmail.com>
2018-04-30 15:35:36 -0700

    [add] seeding userUpdates

--- server/database/SQL/testData.jsx
+++ server/database/SQL/testData.jsx
@@ -36,12 +36,66 @@ const seedUsers = [
     username: 'charizard',
     password: 'charizard',
   },
+];
+
+const userUpdates = [
+  {
+    username: 'pikachu',
+    data: {
+      level: 6,
+      currentEXP: 4000,
+      nextLevelEXP: 10000,
+      wins: 80,
+      losses: 0,
+    }
+  },
+  {
+    username: 'squirtle',
+    data: {
+      level: 3,
+      currentEXP: 2000,
+      nextLevelEXP: 2500,
+      wins: 40,
+      losses: 10,
+    }
+  },
+  {
+    username: 'bulbasaur',
+    data: {
+      level: 4,
+      currentEXP: 2500,
+      nextLevelEXP: 3000,
+      wins: 50,
+      losses: 17,
+    }
+  },
+  {
+    username: 'charizard',
+    data: {
+      level: 10,
+      currentEXP: 20000,
+      nextLevelEXP: 300000,
+      wins: 100,
+      losses: 2,
+    }
+  },
 ]
 
-const seedData = async () => {
+const createUsers = async () => {
   for (let i = 0; i < seedUsers.length; i++) {
-    await createUserHelper(seedUsers[i], (result) => console.log(result));
+    await createUserHelper(seedUsers[i], (result) => console.log(seedUsers[i].username, ' added to DB'));
   }
+}
+
+const updateUsers = async () => {
+  for (let i = 0; i < userUpdates.length; i++) {
+    await updateUserHelper(userUpdates[i], (result) => console.log(userUpdates[i].username, ' info updated'))
+  }
+}
+
+const seedData = async () => {
+  await createUsers();
+  await updateUsers();
 }
 
 seedData();
