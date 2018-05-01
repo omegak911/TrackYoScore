42ad38aed6a3ed59578d9eb147d9ce15b55004a0
Kin Chan <xkinchanx@gmail.com>
2018-05-01 10:47:00 -0700

    [add] client component landing page

---
+++ client/src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Landing from './Landing/Landing';
+
+const App = () => {
+
+  return (
+    <div>
+      <BrowserRouter>
+          <Switch>
+            <Route exact path='/' component={Landing} />
+          </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Landing/Landing.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Landing extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        Hello from Landing Page
+      </div>
+    )
+  }
+}
+
+export default Landing;
