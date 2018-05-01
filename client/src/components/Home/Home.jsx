f8fa7e7fecfbaecb9f49ca9778affceb305328f5
Kin Chan <xkinchanx@gmail.com>
2018-05-01 15:24:28 -0700

    [add] skeleton for homepage

---
+++ client/src/components/Home/Home.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  showTreasure = () => {
+    console.log(this.state);
+    console.log(this.props);
+  }
+
+  render() {
+    return (
+      <div>
+        <button type="button" onClick={this.showTreasure}>**********</button>
+        Hello from HomePage
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    user_data: state.user_data,
+  }
+}
+
+export default connect(mapStateToProps)(Home);
