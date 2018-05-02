c081877a7d606710abf9aa049ad0f4a2f058ba6d
Kin Chan <xkinchanx@gmail.com>
2018-05-01 19:06:44 -0700

    [add] skeleton for profile and search components

---
+++ client/src/components/Search/Search.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { searchedUsers } from '../../redux/actions';
+
+class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userQuery: '',
+    }
+  }
+
+  handleEntry = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState({ [name]: value });
+  }
+
+  findUsers = () => {
+    const options = {
+      params: {
+        userQuery,
+      }
+    }
+
+    axios
+      .get('/api/user/search', options)
+      .then(({ data }) => {
+        // this.props.searchedUsers();
+        console.log(data)
+      })
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.findUsers}>
+          <input name="userQuery" placeholder="search users" type="text" onChange={e => this.handleEntry(e)}/>
+          <button type="button" onClick={this.findUsers}>search</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+const matchDispatchToProps = dispatch => {
+  return bindActionCreators({
+    searchedUsers,
+  }, dispatch);
+}
+
+export default connect(null, matchDispatchToProps)(Search);
