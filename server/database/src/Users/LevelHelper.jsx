d922b68e8d7cf2ec4499c79d11468c0cd27faa55
Kin Chan <xkinchanx@gmail.com>
2018-05-02 15:35:21 -0700

    [add] function to determine EXP update

---
+++ server/database/src/Users/LevelHelper.jsx
@@ -0,0 +1,29 @@
+const increaseNextLevelCap = ({ level, nextLevelEXP }) => {
+  level += 1;
+
+  if (level < 11) {
+    nextLevelEXP += 100;
+  } else if (level < 21) {
+    nextLevelEXP += 200;
+  } else if (level < 31) {
+    nextLevelEXP += 400;
+  } else if (level < 41) {
+    nextLevelEXP += 600;
+  } else if (level < 51) {
+    nextLevelEXP += 800;
+  } else if (level < 61) {
+    nextLevelEXP += 1200;
+  } else if (level < 71) {
+    nextLevelEXP += 1600;
+  } else if (level < 81) {
+    nextLevelEXP += 2000;
+  } else if (level < 91) {
+    nextLevelEXP += 2400;
+  } else if (level < 100) {
+    nextLevelEXP += 2800;
+  }
+
+  return { level, nextLevelEXP }
+}
+
+export default increaseNextLevelCap;
