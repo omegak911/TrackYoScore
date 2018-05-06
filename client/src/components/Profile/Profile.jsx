0ba99d9af23d671b49eed244120a0f746f723c59
Kin Chan <xkinchanx@gmail.com>
2018-05-06 15:19:30 -0700

    [add] rendering clicked user profile and stats

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -4,8 +4,12 @@ import axios from 'axios';
 class Profile extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
-    console.log(this.props)
+    this.state = {
+      username: '',
+      level: '',
+      wins: 0,
+      losses: 0,
+    };
   }
 
   componentDidMount() {
@@ -13,25 +17,57 @@ class Profile extends Component {
 
     //one condition if user goes to own profile
 
-
-
     //2nd condition if user goes to another profile
     let options = {
       params: {
         id: state.user.id
       }
     }
-
+    
     axios
       .get('/api/user/profile', options)
-      .then(({ data }) => console.log('profile result: ', data))
+      .then(({ data }) => {
+        const { username, level, wins, losses } = data;
+        this.setState({ username, level, wins, losses })
+      })
       .catch(err => console.log(err));
   }
 
+  button = () => {
+    console.log(this.state);
+  }
+
   render() {
+    const { username, level, wins, losses } = this.state;
+
     return (
       <div>
         welcome to Profile
+        <button onClick={this.button}>*********</button>
+        <div className="profileImage">
+          Profile Image
+        </div>
+
+        <div className="profileStats">
+          username: { username }
+          <br/>
+          level: { level }
+          <br/>
+          wins: { wins }
+          <br/>
+          losses { losses }
+        </div>
+
+        <div className="bio">
+          bio
+        </div>
+
+        <div>
+
+        </div>
+
+
+
       </div>
     )
   }
