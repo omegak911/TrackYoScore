77e9eb7b54d9261370911f747cebed5f36b641b0
Kin Chan <xkinchanx@gmail.com>
2018-05-09 21:54:12 -0700

    [add] server-side handling to accept friend requests

--- server/database/src/Friends/FriendHelper.jsx
+++ server/database/src/Friends/FriendHelper.jsx
@@ -1,17 +1,25 @@
 import { Friends, FriendRequests } from '../../SQL/index';
 
-const addFriendHelper = ({ userId, friendId }, callback) =>
+const addFriendHelper = ({ friendId, userId }, callback) => {
   Friends.create({
+    userId: friendId,
+    friendId: userId
+  })
+  .then(() => console.log('created friend'))
+  .catch(err => console.log(err));
+
+  return Friends.create({
     userId,
     friendId,
   })
   .then(() => callback())
   .catch(err => console.log(err));
+}
 
-const friendRequestHelper = ({ requestorId, requesteeId }, callback) =>
+const friendRequestHelper = ({ friendId, userId }, callback) =>
   FriendRequests.create({
-    requestorId,
-    requesteeId,
+    friendId,
+    userId,
   })
   .then(result => callback(result))
   .catch(err => console.log(err));
