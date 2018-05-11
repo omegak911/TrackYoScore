9c095379b27c7f8acf59873dbfa4fba40b63865e
Kin Chan <xkinchanx@gmail.com>
2018-05-11 10:11:10 -0700

    [add] displaying game results

--- client/src/components/History/History.jsx
+++ client/src/components/History/History.jsx
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import axios from 'axios';
 
+import './History.scss';
+
 class History extends Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      challengeHist: [],
+    }
   }
 
   componentDidMount() {
@@ -20,14 +24,21 @@ class History extends Component {
 
     axios
       .get('/api/history/history', options)
-      .then(({ data }) => console.log(data))
+      .then(({ data }) => this.setState({ challengeHist: data }))
       .catch(err => console.log(err));
   }
 
   render() {
+    let { challengeHist } = this.state;
+
     return (
       <div>
         Hello from history
+        {challengeHist.map((match, index) =>
+          <div key={index} className="historyResult">
+            {match.playerScore.map((player, index) => 
+              <span key={index} className="historyResultScores">{player.username} {player.score === 10 ? 'Win' : 'Lose'}</span>)}
+          </div>)}
       </div>
     )
   }
