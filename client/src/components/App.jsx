c45e03ffc3b7cd1477baac9ad358b43a665b607b
Kin Chan <xkinchanx@gmail.com>
2018-05-18 16:04:38 -0700

    [add] componentDidMount to check if user had already logged in

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import axios from 'axios';
 
 import History from './History/History';
 import Home from './Home/Home';
@@ -8,24 +11,61 @@ import Nav from './Nav/Nav';
 import Profile from './Profile/Profile';
 import UserSearchResults  from './Search/UserSearchResult';
 
-const App = () => {
+import { updateUserData, updatePendingFriendRequests, updatePendingHistConfirmations, updateFriendList } from '../redux/actions'; 
 
-  return (
-    <div>
-      <BrowserRouter>
-          <div>
-            <Nav />
-            <Switch>
-              <Route exact path='/' component={Landing} />
-              <Route path='/history' component={History} />
-              <Route path='/home' component={Home} />
-              <Route path='/profile' component={Profile} />
-              <Route path='/userSearchResults' component={UserSearchResults} />
-            </Switch>
-          </div>
-      </BrowserRouter>
-    </div>
-  );
+class App extends Component {
+
+  componentDidMount() {
+    const { updateUserData, updatePendingFriendRequests, updatePendingHistConfirmations, updateFriendList } = this.props;
+
+    axios
+      .get('/api/auth/isLoggedIn')
+      .then(({ data }) => {
+        console.log('App CDM success')
+        console.log(data)
+        if (data.username) {
+          updateUserData(data);
+          updateFriendList(data.friendsList);
+          updatePendingFriendRequests(data.friendRequests);
+          updatePendingHistConfirmations(data.confirmationNeeded);
+        } else {
+//somehow send to login?
+          console.log('no data')
+        }
+      })
+      .catch(err => {
+        console.log('App CDM error')
+        console.log(err);
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+            <div>
+              <Nav />
+              <Switch>
+                <Route exact path='/' component={Landing} />
+                <Route path='/history' component={History} />
+                <Route path='/home' component={Home} />
+                <Route path='/profile' component={Profile} />
+                <Route path='/userSearchResults' component={UserSearchResults} />
+              </Switch>
+            </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
-export default App;
+const matchDispatchToProps = dispatch => {
+  return bindActionCreators({
+    updateUserData, 
+    updatePendingFriendRequests, 
+    updatePendingHistConfirmations, 
+    updateFriendList
+  }, dispatch)
+}
+
+export default connect(null, matchDispatchToProps)(App);
