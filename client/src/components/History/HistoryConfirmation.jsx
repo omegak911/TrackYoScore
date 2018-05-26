84600701fb6c287b15ccfcc0a979a7d788be72ba
Kin Chan <xkinchanx@gmail.com>
2018-05-25 18:59:41 -0700

    [add] css to partition confirmation display

--- client/src/components/History/HistoryConfirmation.jsx
+++ client/src/components/History/HistoryConfirmation.jsx
@@ -50,17 +50,32 @@ class HistoryConfirmation extends Component {
         {confirmationModal &&
           <div className="modal">
             <div className="veilOfDarkness" onClick={this.showPendingConfirmations}>
-              <div className="confirmationsPending" onClick={this.stopPropagation}>
-                {histConfirmationsPending.map((data, index) => 
-                  <div key={index}>
-                    {Object.keys(data.confirmation.playerScore).map((user, i) => 
-                      <span className="historyResultScores" key={i}>
-                        {data.confirmation.playerScore[user].username} {data.confirmation.playerScore[user].score === 10 ? 'Win' : 'Loss'}
-                      </span>
-                    )}
+              <div className="confirmationsOuterContainer" onClick={this.stopPropagation}>
+                <div className="confirmationsInnerContainer">
+                {histConfirmationsPending.map((match, index) => 
+                  <div key={index} className="match">
+                    <div className="matchGame">
+                      <img className="gameImage" src={match.confirmation.game.image} />
+                      <div>{match.confirmation.game.title}</div>
+                    </div>
+                    <div className="matchScores">
+                      <div className="confirmationStats">
+                        <span>Player</span>
+                        {' '}
+                        <span className="confirmationsPlayerscore">Result</span>
+                      </div>
+                      <div className="confirmationStats">
+                        {Object.keys(match.confirmation.playerScore).map((user, i) => 
+                          <div className="historyResultScores" key={i}>
+                            <span>{match.confirmation.playerScore[user].username}</span> <span className="confirmationsPlayerscore">{match.confirmation.playerScore[user].score === 10 ? 'Win' : 'Loss'}</span>
+                          </div>
+                        )}
+                      </div>
+                    </div>
                     <button type="button" onClick={() => this.validateConfirmation(index)}>validate</button>
                   </div>
                 )}
+                </div>
               </div>
             </div>
           </div>
