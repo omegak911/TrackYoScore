f7b03a990408b6ef717aad183dbdc978b80d4999
Kin Chan <xkinchanx@gmail.com>
2018-07-02 20:58:00 -0700

    [add] server route to add game from cloudinary to db

--- server/database/src/Games/GameCtrl.jsx
+++ server/database/src/Games/GameCtrl.jsx
@@ -3,11 +3,17 @@ import { addGameHelper, fetchGameHelper } from './GameHelper';
 const addGame = (req, res) => {
   //req.body should contain title and image url
   //check if user is logged in before doing anything
-  addGameHelper(req.body, (result) => {
-    console.log('addGame result: ', result);
-
-    res.status(201).send('Game added')
-  })
+  const { id } = req.session.passport.user;
+  if (id) {
+    addGameHelper(req.body, (result) => {
+      console.log('addGame result: ', result);
+  
+      res.status(201).send('Game added')
+    })
+  } else {
+    res.redirect('/');
+  }
+  
 }
 
 const fetchGame = (req, res) => {
