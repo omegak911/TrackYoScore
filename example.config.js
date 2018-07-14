5cc8549ba731f74b4e97e564db5bc961c432dcef
Kin Chan <xkinchanx@gmail.com>
2018-07-14 09:17:24 -0700

    [add] example config file

---
+++ example.config.js
@@ -0,0 +1,7 @@
+const TO_THE_CLOUD = //___API KEY___
+const PIC_PRESET_NAME = //___preset for profile pics___
+const GAME_PRESET_NAME = //___preset for game pics___
+const CLOUD_URL = //___URL to cloudinary___
+const CLOUD_NAME = //*___cloudinary username___
+
+export { TO_THE_CLOUD, PIC_PRESET_NAME, GAME_PRESET_NAME, CLOUD_URL, CLOUD_NAME };
